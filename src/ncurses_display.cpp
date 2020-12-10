b8f18183be34df18525d57ae4a77de39ffaeff7b
Agnieszka <agnieszkalasota1994@gmail.com>
2020-12-10 21:48:04 +0100

    :lipstick: RAM display fixed

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -72,14 +72,21 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
   wattroff(window, COLOR_PAIR(2));
   for (int i = 0; i < n; ++i) {
     mvwprintw(window, ++row, pid_column, "      ");
+
     mvwprintw(window, row, pid_column, to_string(processes[i].Pid()).c_str());
     mvwprintw(window, row, user_column, "         ");
+
     mvwprintw(window, row, user_column, processes[i].User().c_str());
     float cpu = processes[i].CpuUtilization() * 100;
+
     mvwprintw(window, row, cpu_column, to_string(cpu).substr(0, 4).c_str());
+
+    mvwprintw(window, row, ram_column, "      ");
     mvwprintw(window, row, ram_column, processes[i].Ram().c_str());
+
     mvwprintw(window, row, time_column,
               Format::ElapsedTime(processes[i].UpTime()).c_str());
+    mvwprintw(window, row, command_column, string(window->_maxx - 49,' ').c_str());
     mvwprintw(window, row, command_column,
               processes[i].Command().substr(0, window->_maxx - 49).c_str());
   }
